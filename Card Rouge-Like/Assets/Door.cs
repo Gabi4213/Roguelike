2466c6b0ad8a4c6dfdd553f55da3698e5badf010
Gabi4213 <71651109+Gabi4213@users.noreply.github.com>
2024-09-21 23:26:18 +0100

    Dungeon generator Door positioning WORKS!!

--- Card Rouge-Like/Assets/Door.cs
+++ Card Rouge-Like/Assets/Door.cs
@@ -5,8 +5,13 @@ public class Door : MonoBehaviour
     public enum DoorDirection { Up, Down, Left, Right }
     public DoorDirection doorDirection;
     public Vector3 spawnPosition; // This should be set according to the direction (e.g., Up is +Y)
+    public Room4 currentRoom;
 
-    private void Awake()
+    public Door adjacentDoor;
+    public Transform oppositeDoorPos;
+    public bool adjustPosition;
+
+    private void Start()
     {
         switch (doorDirection)
         {
@@ -23,5 +28,26 @@ public class Door : MonoBehaviour
                 spawnPosition = transform.position + new Vector3(1, 0, 0);
                 break;
         }
+
+        if (adjustPosition && adjacentDoor) 
+        {
+            switch (doorDirection)
+            {
+                case DoorDirection.Up:
+                    transform.position = adjacentDoor.oppositeDoorPos.position + new Vector3(0, 0, 0);
+                    break;
+                case DoorDirection.Down:
+                    transform.position = adjacentDoor.oppositeDoorPos.position + new Vector3(0, 0, 0);
+                    break;
+                case DoorDirection.Left:
+                    transform.position = adjacentDoor.oppositeDoorPos.position + new Vector3(0, 0, 0);
+                    break;
+                case DoorDirection.Right:
+                    transform.position = adjacentDoor.oppositeDoorPos.position + new Vector3(0, 0, 0);
+                    break;
+            }
+        }
+
+
     }
 }
--- Card Rouge-Like/Assets/Room4.cs
+++ Card Rouge-Like/Assets/Room4.cs
@@ -24,6 +24,17 @@ public class Room4 : MonoBehaviour
             if (i < leftRooms.Count)
             {
                 leftDoors[i].SetActive(true);
+                // Set the adjacent door
+                Door leftDoorScript = leftDoors[i].GetComponent<Door>();
+                if (leftDoorScript != null && leftRooms[i] != null)
+                {
+                    // Check if the neighbor has right doors
+                    if (leftRooms[i].rightDoors.Count > 0)
+                    {
+                        Door adjacentLeftDoor = leftRooms[i].rightDoors[0].GetComponent<Door>(); // Assuming the first door
+                        leftDoorScript.adjacentDoor = adjacentLeftDoor;
+                    }
+                }
             }
             else
             {
@@ -36,7 +47,20 @@ public class Room4 : MonoBehaviour
         {
             if (i < rightRooms.Count)
             {
-                rightDoors[i].SetActive(true);
+                if (rightDoors[i])
+                {
+                    rightDoors[i].SetActive(true);
+                    // Set the adjacent door
+                    Door rightDoorScript = rightDoors[i].GetComponent<Door>();
+                    if (rightDoorScript != null && rightRooms[i] != null)
+                    {
+                        if (rightRooms[i].leftDoors.Count > 0)
+                        {
+                            Door adjacentRightDoor = rightRooms[i].leftDoors[0].GetComponent<Door>();
+                            rightDoorScript.adjacentDoor = adjacentRightDoor;
+                        }
+                    }
+                }
             }
             else
             {
@@ -50,6 +74,16 @@ public class Room4 : MonoBehaviour
             if (i < topRooms.Count)
             {
                 topDoors[i].SetActive(true);
+                // Set the adjacent door
+                Door topDoorScript = topDoors[i].GetComponent<Door>();
+                if (topDoorScript != null && topRooms[i] != null)
+                {
+                    if (topRooms[i].bottomDoors.Count > 0)
+                    {
+                        Door adjacentTopDoor = topRooms[i].bottomDoors[0].GetComponent<Door>();
+                        topDoorScript.adjacentDoor = adjacentTopDoor;
+                    }
+                }
             }
             else
             {
@@ -63,16 +97,24 @@ public class Room4 : MonoBehaviour
             if (i < bottomRooms.Count)
             {
                 bottomDoors[i].SetActive(true);
+                // Set the adjacent door
+                Door bottomDoorScript = bottomDoors[i].GetComponent<Door>();
+                if (bottomDoorScript != null && bottomRooms[i] != null)
+                {
+                    if (bottomRooms[i].topDoors.Count > 0)
+                    {
+                        Door adjacentBottomDoor = bottomRooms[i].topDoors[0].GetComponent<Door>();
+                        bottomDoorScript.adjacentDoor = adjacentBottomDoor;
+                    }
+                }
             }
             else
             {
                 bottomDoors[i].SetActive(false);
             }
         }
-
-        // Enable doors based on actual adjacency
-        EnableDoorsBasedOnAdjacency();
     }
+
 
     private void EnableDoorsBasedOnAdjacency()
     {
